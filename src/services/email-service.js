a1b4ce4f41918a853e978a89504e2b8a37291cd3
BiplawSingh <biplawgaurav@gmail.com>
2024-08-31 19:16:08 +0530

    Added notification cron

---
+++ src/services/email-service.js
@@ -0,0 +1,54 @@
+const sender = require('../config/emailConfig');
+const TicketRepository = require('../repository/ticket-repository');
+
+const ticketRepository = new TicketRepository();
+
+const sendBasicEmail = async (mailFrom, mailTo, mailSubject, mailBody) => {
+    try {
+        const response =  await sender.sendMail({
+                                from: mailFrom,
+                                to: mailTo,
+                                subject: mailSubject,
+                                text: mailBody
+                            });
+        console.log(response);
+    } catch(error) {
+        console.log(error);
+    }  
+}
+
+const fetchPendingEmails = async (timestamp) => {
+    try {
+        const response =  await ticketRepository.get({status: 'PENDING'});
+        return response;
+    } catch(error) {
+        console.log(error);
+    }  
+}
+
+const updateTicket = async (ticketId, data) => {
+    try {
+        const response = await ticketRepository.update(ticketId, data);
+        return response;
+    } catch(error) {
+        console.log(error);
+    }
+}
+
+const createNotification = async (data) => {
+    try {
+        const response = await ticketRepository.create(data);
+        return response;
+    } catch(error) {
+        console.log(error);
+    }
+}
+
+
+module.exports = { 
+                    sendBasicEmail, 
+                    fetchPendingEmails,
+                    createNotification,
+                    updateTicket
+                 }
+    
