2372ab5bdbdd096cc5d5ebe3b1da1f3b06c7336e
BiplawSingh <biplawgaurav@gmail.com>
2024-09-01 03:35:48 +0530

    Message queue initial setup

--- src/index.js
+++ src/index.js
@@ -2,20 +2,24 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const { PORT } = require('./config/serverConfig');
+const { createChannel } = require('./utils/messageQueue');
 
 const TicketController = require('./controllers/ticket-controller');
 const jobs = require('./utils/job');
+const m = require('./models/notificationticket');
 
-const setupAndStartServer = () => {
+const setupAndStartServer = async () => {
     const app = express();
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: true}));
+    
+    const channel = await createChannel();
 
     app.post('/api/v1/tickets', TicketController.create);
 
     app.listen(PORT, () => {
         console.log(`Server started on port ${PORT}`);
-        jobs();
+        // jobs();
     });
 }
 
