aa15127298bd3db9593b9347fe39486a2bc6dae0
BiplawSingh <biplawgaurav@gmail.com>
2024-09-02 22:38:11 +0530

    Added subscribe events logic

--- src/index.js
+++ src/index.js
@@ -2,20 +2,23 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const { PORT } = require('./config/serverConfig');
-const { createChannel } = require('./utils/messageQueue');
 
 const TicketController = require('./controllers/ticket-controller');
+const EmailService = require('./services/email-service'); 
 const jobs = require('./utils/job');
-const m = require('./models/notificationticket');
+
+const { subscribeMessage, createChannel } = require('./utils/messageQueue');
+const { REMINDER_BINDING_KEY } = require('./config/serverConfig');
 
 const setupAndStartServer = async () => {
     const app = express();
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: true}));
-    
-    const channel = await createChannel();
 
     app.post('/api/v1/tickets', TicketController.create);
+
+    const channel = await createChannel();
+    subscribeMessage(channel, EmailService.subscribeEvents, REMINDER_BINDING_KEY);
 
     app.listen(PORT, () => {
         console.log(`Server started on port ${PORT}`);
